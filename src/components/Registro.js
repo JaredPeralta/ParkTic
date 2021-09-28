0a194c10d6b2cfbe0f4f23b1eb848908de9a996b
JaredPeralta <jared.peralta14@outlook.com>
2021-09-28 14:01:37 -0500

    Calculate Price

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -10,6 +10,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 const Registro = () => {
     
+    const valorHora = 100
     const [value, setValue] = useState("");
     const [placas, setPlaca] = useState([]);
     
@@ -18,12 +19,20 @@ const Registro = () => {
     }
 
     const handleClick = () => {
+
         if(value.length !== 7){
             alert("No es una placa de carro")
         }else{
-            setPlaca([...placas,value])
+            const fecha = new Date();
+            const existe = placas.find((placa) => placa.placa === value);
+            if(!existe){
+                setPlaca([...placas,{placa:value,horaEntrada:fecha}])
+            }else{
+                existe.horaSalida = fecha
+                existe.precio = (parseInt((existe.horaSalida - existe.horaEntrada)/60000)+1)*valorHora
+                setPlaca([...placas]);
+            }
         }
-        console.log(placas)
     }
 
     return(
@@ -49,12 +58,26 @@ const Registro = () => {
                 </Col>
             </Row>
 
-            <Row>
-                <ListGroup>
-                    {placas.map((placa) => {
-                        return(<ListGroup.Item>{placa}</ListGroup.Item>)
-                    })}
-                </ListGroup>
+            <Row className="d-flex justify-content-center align-items-center mt-3 mb-3">
+                <Col xs={6}>
+                    <ListGroup className="text-center">
+                        {placas.map((placa) => {
+                            if(placa.horaSalida){
+                                return(
+                                    <ListGroup.Item>
+                                        {placa.placa} - {placa.horaEntrada.toLocaleString()} - {placa.horaSalida.toLocaleString()} - ${placa.precio}
+                                    </ListGroup.Item>
+                                )
+                            }else{
+                                return(
+                                    <ListGroup.Item>
+                                        {placa.placa} - {placa.horaEntrada.toLocaleString()}
+                                    </ListGroup.Item>
+                                )
+                            }
+                        })}
+                    </ListGroup>
+                </Col>
             </Row>
         </Container>
     );
